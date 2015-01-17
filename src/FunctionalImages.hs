446ae879547a34094512104bd8baa4820d50e9c9
roland <rsx@bluewin.ch>
2015-01-17 18:20:34 +0100

    Next image: checker (chessboard)

--- src/FunctionalImages.hs
+++ src/FunctionalImages.hs
@@ -9,6 +9,7 @@
 module FunctionalImages (
    -- * image creating functions of Conal Eliott
    vstrip,
+   checker,
    -- * interface functions to JuicyPixels
    generateBWImageR2
    ) where
@@ -18,5 +19,8 @@ import FunctionalImagesBase
 
 -- | image: Generate an vertical black strip
 vstrip :: Point -> Bool
-vstrip (x, y) = abs x < 0.5 
- 
+vstrip (x, y) = abs x < 0.5
+
+-- | image: Generate a chess board
+checker :: Point -> Bool
+checker (x, y) = even $ floor x + floor y
