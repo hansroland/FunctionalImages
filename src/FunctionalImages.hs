18a697de1e7394fe5326c5eb5c9a6ab6f0ca346e
roland <rsx@bluewin.ch>
2015-01-18 12:50:16 +0100

    Added alternate rings

--- src/FunctionalImages.hs
+++ src/FunctionalImages.hs
@@ -10,6 +10,7 @@ module FunctionalImages (
    -- * image creating functions of Conal Eliott
    vstrip,
    checker,
+   altRings,
    -- * interface functions to JuicyPixels
    generateBWImageR2
    ) where
@@ -24,3 +25,11 @@ vstrip (x, y) = abs x < 0.5
 -- | image: Generate a chess board
 checker :: Point -> Bool
 checker (x, y) = even $ floor x + floor y
+
+-- | distance to the origin of the coordiante system
+distO :: Point -> Float
+distO (x, y) = sqrt $ x**2 + y**2
+
+-- | alternate concentric Rings
+altRings :: Point -> Bool
+altRings  = even . floor . distO
