25ab7812947c0b34c892228ac33b423257a661d4
roland <rsx@bluewin.ch>
2015-01-18 13:03:49 +0100

    Added: polar Checker

--- src/FunctionalImages.hs
+++ src/FunctionalImages.hs
@@ -11,12 +11,17 @@ module FunctionalImages (
    vstrip,
    checker,
    altRings,
+   polarChecker,
    -- * interface functions to JuicyPixels
    generateBWImageR2
    ) where
 
 import Codec.Picture
 import FunctionalImagesBase
+
+-- ----------------------------------------------------------------------------
+-- First simple images
+-- ----------------------------------------------------------------------------
 
 -- | image: Generate an vertical black strip
 vstrip :: Point -> Bool
@@ -33,3 +38,24 @@ distO (x, y) = sqrt $ x**2 + y**2
 -- | alternate concentric Rings
 altRings :: Point -> Bool
 altRings  = even . floor . distO
+
+-- ----------------------------------------------------------------------------
+-- Polar Coordinates
+-- ----------------------------------------------------------------------------
+
+-- | Polar Coordinates
+type PolarPoint = (Float, Float)
+
+-- | Convert from Polar to Cartesian coordinates
+fromPolar :: PolarPoint -> Point
+fromPolar (ρ, θ) = (ρ * cos θ, ρ * sin θ)
+
+-- | Convert from Cartesian to Polar coordinates
+toPolar:: Point -> PolarPoint
+toPolar (x, y) = (distO (x, y), atan2 y x)
+
+-- | Polar Checkboard
+polarChecker :: Int -> Point -> Bool
+polarChecker n = checker . sc . toPolar
+   where
+     sc (r,a) = (r,a * fromIntegral n / pi)
