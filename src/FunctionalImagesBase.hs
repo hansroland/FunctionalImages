7d1a8d6167b15ab791c9a2a0e2ab771aebded0cd
roland <rsx@bluewin.ch>
2016-02-14 21:57:24 +0100

    Refactor functions: Remove generateGrayImageR2, generateBWImageR2

--- src/FunctionalImagesBase.hs
+++ src/FunctionalImagesBase.hs
@@ -8,13 +8,15 @@
 --
 -- ---------------------------------------------------------------------------
 
-module FunctionalImagesBase(
-    Point,
-    IPoint,
-    Frac,
-    generateBWImageR2,
-    generateBWImageI2,
-    generateGrayImageR2
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module FunctionalImagesBase
+    ( FImage
+    , Point
+    , IPoint
+    , Frac
+    , generateImageR2
+    , generateBWImageI2
     ) where
 
 import Codec.Picture
@@ -28,6 +30,9 @@ type IPoint = (Int, Int)
 -- type fraction means numbers between 0 and 1
 type Frac = Float
 
+-- | a type for the Functional Images (note FImage to avoid name clashes)
+type FImage a = Point -> a
+
 -- a little helper function to restict a value into an interal
 inInterval :: Ord a => a -> a -> a -> a
 inInterval val low high
@@ -35,17 +40,31 @@ inInterval val low high
    | val > high = high
    | otherwise  = val
 
+-- | a class to convert to JuicyPixel colors
+class ToPixelRGB8 a where
+  toPixelRGB8 :: a -> PixelRGB8
+
+instance ToPixelRGB8 Bool where
+  toPixelRGB8 False  = PixelRGB8 255 255 255
+  toPixelRGB8 True = PixelRGB8   0   0   0
+
+instance ToPixelRGB8 Frac where
+  toPixelRGB8 f = PixelRGB8 ff ff ff
+      where
+        ff = frac2pixel8 f
+        frac2pixel8 f = floor $ 255 * inInterval f 0 1
+
 -- | Generate an image in the Real X Real space
-generateImageR2 :: Pixel a => (Point -> a)  -- Coordinate Rendering function
+generateImageR2 :: ToPixelRGB8 a => (Point -> a)  -- Coordinate Rendering function
          -> Point                           -- lower left corner
          -> Point                            -- upper right corner
          -> Int                              -- width in pixel
-         -> Image a                          -- Resulting picture
+         -> Image PixelRGB8                          -- Resulting picture
 generateImageR2 coordRenderer (x0,y0) (x1,y1) width = generateImage pixelRenderer width height
     where
       pixelSize = (x1 - x0) / fromIntegral width
       height = floor $ (y1 - y0) / (x1 - x0) * fromIntegral width
-      pixelRenderer ix iy = coordRenderer (x0 + fromIntegral ix * pixelSize, y1 - fromIntegral iy * pixelSize)
+      pixelRenderer ix iy = toPixelRGB8 $ coordRenderer (x0 + fromIntegral ix * pixelSize, y1 - fromIntegral iy * pixelSize)
 
 
 -- | Generate an image in the Int X Int space
@@ -60,55 +79,9 @@ generateImageI2 coordRenderer (x0,y0) (x1,y1) =
     where
       pixelRenderer ix iy = coordRenderer (x0 + ix, y1 - iy)
 
--- A set of some simple colors
-rgb8White = PixelRGB8 255 255 255
-rgb8Black = PixelRGB8 0 0 0
-
--- ---------------------------------------------------------------------------
--- Black and White images
--- ---------------------------------------------------------------------------
-
--- | General function to generate a black and white image in R2
-generateBWImageR2 :: (Point -> Bool)            -- black white function in R2
-         -> Point                               -- lower left corner
-         -> Point                               -- upper right corner
-         -> Int                                 -- width in pixel
-         -> Image PixelRGB8                     -- Resulting picture
-generateBWImageR2 bwRenderer = generateImageR2 (bool2rgb8 . bwRenderer)
-
-
 -- | Generate an
 generateBWImageI2 :: (IPoint -> Bool)            -- black white function in R2
          -> IPoint                               -- lower left corner
          -> IPoint                               -- upper right corner
          -> Image PixelRGB8                      -- Resulting picture
-generateBWImageI2 bwRenderer = generateImageI2 (bool2rgb8 . bwRenderer)
-
-
--- A litte support function for generateBWImageR2
--- Convert Boolean values to Colors
-bool2rgb8 :: Bool -> PixelRGB8
-bool2rgb8 True  = rgb8Black
-bool2rgb8 False = rgb8White
-
--- ----------------------------------------------------------------------------
--- Gray images
--- ----------------------------------------------------------------------------
-
--- | generate a grayscale image in R2
-generateGrayImageR2 :: (Point -> Frac)
-         -> (Float, Float)
-         -> (Float, Float)
-         -> Int
-         -> Image PixelRGB8
-generateGrayImageR2 renderer = generateImageR2 (frac2rgb8 . renderer)
-
--- a little support function to generate grey images
-frac2rgb8 :: Frac -> PixelRGB8
-frac2rgb8 f = PixelRGB8 ff ff ff
-    where
-      ff = frac2pixel8 f
-
--- function to convert a Fraction between 0 and 1 to an Integer betwenn 0 and255
-frac2pixel8 :: Frac -> Pixel8
-frac2pixel8 f = floor $ 255 * inInterval f 0 1
+generateBWImageI2 bwRenderer = generateImageI2 (toPixelRGB8 . bwRenderer)
